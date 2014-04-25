7fa56c6988d7cac778a8c16730d58d9f1648d50d
Sida Wang <sidaw@google.com>
2014-04-25 11:13:07 -0700

    add module

---
+++ module.m
@@ -0,0 +1,17 @@
+classdef module
+ % write a description of the class here.
+properties
+     input
+     output
+     parameters
+end
+
+methods
+    function output = updateOutput(input)
+        output = input
+    end
+
+    function grad = computeGradient(dfdo)
+end
+
+end
