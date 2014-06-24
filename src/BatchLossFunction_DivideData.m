165880b70bb6c44c040796c116aa9f8bd51bed9e
Sida Wang <sidaw@google.com>
2014-06-24 11:26:28 -0700

    add divide data batchloss and testnewcapsules

---
+++ src/BatchLossFunction_DivideData.m
@@ -0,0 +1,29 @@
+function [ loss, grad ] = BatchLossFunction_DivideData(params, X, y, nn, lossname, options)
+    [lossfunc, lossgrad] = getLoss(lossname);
+    nn.setparams(params);
+    
+    numexample = size(X,2);
+    batchsize = 1000;
+    
+    loss = 0;
+    grad = 0;
+    for startind=1:batchsize:numexample
+        endind = min(startind + batchsize - 1, numexample);
+        
+        Xc = X(:, startind:endind);
+        yc = y(:, startind:endind);
+        outputc = nn.forward(Xc);
+        lossesc = lossfunc(outputc, yc);
+        loss = loss + sum(lossesc);
+        
+        dLdoutc = lossgrad(outputc, yc);
+        dldinc = nn.backward(dLdoutc);
+        grad = grad + nn.getgrad();
+    
+    end
+   
+    lambda = parseOption(options, 'lambdaL2', 1e-3);
+    loss = loss + 0.5*lambda*sum(params.*params);
+    grad = grad + lambda * params;
+end
+
